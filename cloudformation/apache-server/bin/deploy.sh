865d07575925bb519a0a8ebab1be48fa801fed90
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-04-01 03:42:07 +0100

    fix shebang

--- cloudformation/apache-server/bin/deploy.sh
+++ cloudformation/apache-server/bin/deploy.sh
@@ -1,4 +1,4 @@
-# !/bin/sh -ex
+#!/bin/sh -ex
 
 aws cloudformation create-stack --stack-name apache-server \
     --template-body file:///Users/abdulrasheedlawal/software-engineering/playground/sre/cloud/aws/playground/cloudformation/apache-server/stack.json \
